19457f6e1f69e0e9b9b70d3f172a6c6919cc0916
Alyona Voinova <nena.2000@mail.ru>
2021-07-18 22:16:45 +0300

    Add animated chicken to menu

---
+++ Assets/Scripts/MenuChickenController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuChickenController : MonoBehaviour
+{
+    private Animator _animator;
+    private string _currentAnimation;
+
+    public float repeat_time;
+    private float _curr_time;
+
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+        _curr_time = repeat_time;
+    }
+    void Update()
+    {
+        _curr_time -= Time.deltaTime;
+        Debug.Log(_curr_time);
+        if (_curr_time <= 0)
+        {
+            StartRandomAnimation();
+            _curr_time = repeat_time ;
+        }
+    }
+
+    void StartRandomAnimation()
+    {
+        _animator.SetBool(_currentAnimation, false);
+        _currentAnimation = _animator.parameters[Random.Range(0, _animator.parameters.Length)].name;
+        _animator.SetBool(_currentAnimation, true);
+    }
+}
