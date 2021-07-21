b34498a85042d8bbc74a779e94f0101804e7bc31
Alyona Voinova <nena.2000@mail.ru>
2021-07-21 20:14:50 +0300

    Bat fixes

--- Assets/Scripts/KillerController.cs
+++ Assets/Scripts/KillerController.cs
@@ -7,18 +7,16 @@ public class KillerController : BaseCharacter
     [SerializeField] private bool isPressed = false;
     [SerializeField] private float distance = 10f;
     [SerializeField] private Rigidbody _killerRigid;
+    [SerializeField] private Rigidbody _slingshotRigid;
     [SerializeField] private SpringJoint _springJoint;
+    [SerializeField] private Animator _animator;
 
-    private Camera _camera;
-    
-    private Animator _animator;
-
+    private Camera _camera;  
 
     private void Start()
     {
 
         id = GetInstanceID();
-        _animator = GetComponent<Animator>();
         _camera = Camera.main;
     }
 
@@ -26,9 +24,15 @@ public class KillerController : BaseCharacter
     {
         if(isPressed)
         {
-            Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance); 
-            Vector3 objPosition = _camera.ScreenToWorldPoint(mousePosition); 
-            _killerRigid.position = objPosition; 
+            Vector3 mousePosition = _camera.ScreenToWorldPoint(Input.mousePosition);
+            if (Vector3.Distance(mousePosition, _slingshotRigid.position) > distance)
+            {
+                _killerRigid.position = _slingshotRigid.position + (mousePosition - _slingshotRigid.position).normalized * distance;
+            } 
+            else 
+            {
+                _killerRigid.position = mousePosition;
+            }           
         }
     }
     private void OnMouseUp()
